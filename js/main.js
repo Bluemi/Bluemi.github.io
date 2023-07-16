ac32b3284f5421d25c225b3ea3e8d13745d1d13b
Bluemi <bruno.schilling@protonmail.ch>
2023-07-16 08:46:11 +0200

    wip event handling

--- js/main.js
+++ js/main.js
@@ -3,13 +3,11 @@ async function main(){
     await pyodide.loadPackage("micropip");
     const micropip = pyodide.pyimport("micropip");
     // await micropip.install("https://raw.githubusercontent.com/Bluemi/Bluemi.github.io/main/wheels/test_module-0.1.0-py3-none-any.whl");
-    const pygameHelper = createPygameHelper();
-    pyodide.registerJsModule("pygame_helper", pygameHelper);
-    await micropip.install("wheels/pygame-0.1.0-py3-none-any.whl")
+    createPygameHelper(pyodide, micropip);
     await micropip.install("wheels/test_module-0.1.0-py3-none-any.whl");
     pyodide.runPython(`
-        import test_module.main
-        test_module.main.main()
+        from test_module.main import main
+        main_instance = main()
     `);
 }
 
